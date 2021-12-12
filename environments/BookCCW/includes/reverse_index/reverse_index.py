857b4f8e9bf8ec9de846dd490bd6e80301aa4e99
mhco0 <mhco@cin.ufpe.br>
2021-12-12 18:52:38 -0300

    Adding attributes field data

--- environments/BookCCW/includes/reverse_index/reverse_index.py
+++ environments/BookCCW/includes/reverse_index/reverse_index.py
@@ -19,17 +19,101 @@ class ReverseIndex(object):
         self.use_compression = compression
         self.doc_id = 0 
         self.dfpostings = {}
+        self.dfattributes = {}
         self.path_to_id = {}
 
-        self.__pre_process_attributs()
-
         if self.use_compression:
+            self.__pre_process_attributes_compression()
             self.__make_compression_postings()
         else:
+            self.__pre_process_attributes_uncompression()
             self.__make_uncompression_postings()
 
-    def __pre_process_attributs(self):
-        pass
+    def __pre_process_attributes_compression(self):
+        with open(self.extration_filename, 'r') as file:
+            data = json.load(file)
+
+        extract_info = ["author", "publisher", "language", "domain"]
+
+        for pair_att_value in data:
+            self.path_to_id[self.doc_id] = self.path_database + '/' + pair_att_value["path"]
+
+            isbn = pair_att_value["isbn"]
+
+            word = "isbn." + isbn
+
+            if word in self.dfattributes:
+                index_range = 0
+                for post_list in self.dfattributes[word]:
+                    index_range += VariableByteCompression.decode(post_list[0])
+
+                if index_range == self.doc_id:
+                    self.dfattributes[word][-1][1] += 1
+                else:
+                    self.dfattributes[word].append([VariableByteCompression.encode(self.doc_id - index_range), 1])
+                    
+            else:
+                self.dfattributes[word] = [[VariableByteCompression.encode(self.doc_id), 1]]
+
+            for info in extract_info:
+                words = self.__tokenize(pair_att_value[info])
+
+                for word in words:
+                    word = info + '.' + word
+                    if word in self.dfattributes:
+                        index_range = 0
+                        for post_list in self.dfattributes[word]:
+                            index_range += VariableByteCompression.decode(post_list[0])
+
+                        if index_range == self.doc_id:
+                            self.dfattributes[word][-1][1] += 1
+                        else:
+                            self.dfattributes[word].append([VariableByteCompression.encode(self.doc_id - index_range), 1])
+                            
+                    else:
+                        self.dfattributes[word] = [[VariableByteCompression.encode(self.doc_id), 1]]
+
+
+            self.doc_id += 1
+
+            
+    def __pre_process_attributes_uncompression(self):
+        with open(self.extration_filename, 'r') as file:
+            data = json.load(file)
+
+        extract_info = ["author", "publisher", "language", "domain"]
+
+        for pair_att_value in data:
+            self.path_to_id[self.doc_id] = self.path_database + '/' + pair_att_value["path"]
+
+            isbn = pair_att_value["isbn"]
+
+            word = "isbn." + isbn
+
+            if word in self.dfattributes:
+                if int.from_bytes(self.dfattributes[word][-1][0], 'little') == self.doc_id:
+                    self.dfattributes[word][-1][1] += 1
+                else:
+                    self.dfattributes[word].append([bytearray(self.doc_id.to_bytes(4, 'little')), 1])
+            else:
+                self.dfattributes[word] = [[bytearray(self.doc_id.to_bytes(4, 'little')), 1]]
+
+            for info in extract_info:
+                words = self.__tokenize(pair_att_value[info])
+
+                for word in words:
+                    word = info + '.' + word
+                    if word in self.dfattributes:
+                        if int.from_bytes(self.dfattributes[word][-1][0], 'little') == self.doc_id:
+                            self.dfattributes[word][-1][1] += 1
+                        else:
+                            self.dfattributes[word].append([bytearray(self.doc_id.to_bytes(4, 'little')), 1])
+                            
+                    else:
+                        self.dfattributes[word] = [[bytearray(self.doc_id.to_bytes(4, 'little')), 1]]
+
+
+            self.doc_id += 1
 
     def __make_compression_postings(self):
         dirs = self.database.list_dirs() 
@@ -71,12 +155,12 @@ class ReverseIndex(object):
                 for word in words:
                     if word in self.dfpostings:
                         
-                        if self.dfpostings[word][-1][0] == self.doc_id:
+                        if int.from_bytes(self.dfpostings[word][-1][0], 'little') == self.doc_id:
                             self.dfpostings[word][-1][1] += 1
                         else:
-                            self.dfpostings[word].append([self.doc_id, 1])
+                            self.dfpostings[word].append([bytearray(self.doc_id.to_bytes(4, 'little')), 1])
                     else:
-                        self.dfpostings[word] = [[self.doc_id, 1]]
+                        self.dfpostings[word] = [[bytearray(self.doc_id.to_bytes(4, 'little')), 1]]
 
                 self.doc_id += 1
 
@@ -113,6 +197,9 @@ class ReverseIndex(object):
 
     def get_document_map(self):
         return self.path_to_id
+
+    def get_attributes_map(self):
+        return self.dfattributes
 
     def get_document_by_id(self, idx):
         if idx in self.path_to_id:
---
+++ environments/BookCCW/test_reverse_index.py
@@ -0,0 +1,37 @@
+from includes.reverse_index.reverse_index import *
+import sys
+
+BOOKS_PATH = "includes/database/books"
+EXTRACT_FILE_PATH = "includes/database/books/data.json"
+COMPRESS_FILENAME = "compressRI.pkl"
+UNCOMPRESS_FILENAME = "uncompressRI.pkl"
+ 
+if __name__ == "__main__":
+    
+    if len(sys.argv) > 1 and sys.argv[1] == "--save-indexes":
+        # without compression
+
+        if len(sys.argv) > 2 and sys.argv[2] == "--uncompress":
+            ri = ReverseIndex(BOOKS_PATH, EXTRACT_FILE_PATH, False)
+            ReverseIndex.save(ri, BOOKS_PATH + '/' + UNCOMPRESS_FILENAME)
+
+        # with compression
+        if len(sys.argv) > 2 and sys.argv[2] == "--compress":
+            ri2 = ReverseIndex(BOOKS_PATH, EXTRACT_FILE_PATH, True)
+            ReverseIndex.save(ri2, BOOKS_PATH + '/' + COMPRESS_FILENAME)
+    
+
+    if len(sys.argv) > 1 and sys.argv[1] == "--load-indexes":
+        # without compression
+        if len(sys.argv) > 2 and sys.argv[2] == "--uncompress":
+            ri = ReverseIndex.load(BOOKS_PATH + '/' + UNCOMPRESS_FILENAME)
+            print(ri.get_postings_dict())
+            print(ri.does_compression())
+            print(ri.get_document_map())
+        
+        # with compression
+        if len(sys.argv) > 2 and sys.argv[2] == "--compress":
+            ri2 = ReverseIndex.load(BOOKS_PATH + '/' + COMPRESS_FILENAME)
+            print(ri2.get_postings_dict())
+            print(ri2.does_compression())
+            print(ri2.get_document_map())
